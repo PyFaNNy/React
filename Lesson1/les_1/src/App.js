e1b43076227b032c436a13d1c10a20208a108893
PyFaNNy <fanny.koob@gmail.com>
2022-11-09 23:00:44 +0300

    add html and css

--- Lesson1/les_1/src/App.js
+++ Lesson1/les_1/src/App.js
@@ -1,19 +1,47 @@
-import Techonologies from "./Techonologies"
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <div>
-        <Techonologies />
-      </div>
-      <div>one div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>react</li>
-          <li>js</li>
-        </ul>
+    <div className="app-wraper">
+      <header className="header">
+        <img src="https://uploads.turbologo.com/uploads/design/hq_preview_image/4551835/draw_svg20211224-8647-1vc0v11.svg.png" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messanges</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://img.freepik.com/free-vector/perspective-japanese-street-neon-lights_52683-44988.jpg?w=2000' style={{height:'250px', width:'100%'}}/>
+        </div>
+        <div>
+          ava + disc
+        </div>
+        <div>
+          My posts
+          <div>
+            Posts
+            <div>
+              POST1
+            </div>
+            <div>
+              POST2
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
