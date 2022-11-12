dfaf8bf61a232cd0c7ad2c4d6e5a7f88f2bd82fa
PyFaNNy <fanny.koob@gmail.com>
2022-11-12 21:11:34 +0300

    Add dialogs template

--- Social NetWork/my_project/src/Componets/Dialogs/Dialogs.jsx
+++ Social NetWork/my_project/src/Componets/Dialogs/Dialogs.jsx
@@ -2,8 +2,26 @@ import style from './Dialogs.module.css'
 
 function Dialogs(props) {
   return (
-    <div>
-      Dialogs
+    <div className={style.dialogs}>
+      <div className={style.dialogsItems}>
+        <div className={style.dialog + ' ' + style.active}>
+          Dmitry
+        </div>
+        <div className={style.dialog}>
+          Artem
+        </div>
+        <div className={style.dialog}>
+          Anton
+        </div>
+      </div>
+      <div className={style.messenges}>
+        <div className={style.messange}>
+          Hi
+        </div>
+        <div className={style.messange}>
+          How are you
+        </div>
+      </div>
     </div>
   )
 }
