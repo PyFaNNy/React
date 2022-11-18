396305c9c9da5ddadcd7f40ee0425b7a91e242df
PyFaNNy <fanny.koob@gmail.com>
2022-11-18 22:11:22 +0300

    event for button

--- Social NetWork/my_project/src/Componets/Profile/MyPosts/MyPosts.jsx
+++ Social NetWork/my_project/src/Componets/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,24 @@
 import style from './MyPosts.module.css'
 import Post from './Post/Posts';
-
+import React from 'react'
 function MyPosts() {
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
   return (
     <div>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
 
         <Post messenge="Hi, how are you" />
